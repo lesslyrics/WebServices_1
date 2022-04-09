aeca3ed02efde04973a1364d14ddc10330bd9b34
Lesslyrics <alina_boshenko@mail.ru>
2022-04-09 18:43:30 +0200

    added url check

---
+++ swagger_server/service/url_parser.py
@@ -0,0 +1,20 @@
+import re
+
+
+def is_valid_URL(url):
+    # Regex to check valid URL
+    regex = ("((http|https)://)(www.)?" +
+             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+             "{2,256}\\.[a-z]" +
+             "{2,6}\\b([-a-zA-Z0-9@:%" +
+             "._\\+~#?&//=]*)")
+
+    p = re.compile(regex)
+
+    if url == None:
+        return False
+
+    if re.search(p, url):
+        return True
+    else:
+        return False
--- swagger_server/service/url_service.py
+++ swagger_server/service/url_service.py
@@ -3,14 +3,11 @@ import tempfile
 
 import shortuuid
 from tinydb import TinyDB, Query
-from tinydb.operations import delete
 from flask import Flask, redirect
-
-from tinydb.middlewares import CachingMiddleware
 from functools import reduce
 from shortuuid import *
 import uuid
-from swagger_server.models import ShortenedUrl
+from swagger_server.service.url_parser import is_valid_URL
 
 db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "urls.json")
@@ -18,6 +15,10 @@ url_db = TinyDB(db_file_path)
 
 
 def add(url=None):
+
+    if not is_valid_URL(url.original_url):
+        return 'Invalid Url', 400
+
     queries = []
     query = Query()
     queries.append(query.original_url == url.original_url)
@@ -59,7 +60,10 @@ def update_by_id(url_id=None, new_url=None):
 
     # url = url_db.get(doc_id=int(url_id))
     if not url:
-        return 'not found', 404
+        return 'Not found', 404
+
+    if not is_valid_URL(new_url.original_url):
+        return 'Invalid Url', 400
 
     ShortenedUrl = Query()
     url_db.update({'original_url': new_url.original_url, 'shortened_url': shortuuid.ShortUUID().random(length=7)}, ShortenedUrl.url_id == url_id)
@@ -71,7 +75,7 @@ def delete_by_id(url_id):
     ShortenedUrl = Query()
     student = url_db.get(ShortenedUrl.url_id == url_id)
     if not student:
-        return 'not found', 404
+        return 'Not found', 404
     url_db.remove(ShortenedUrl.url_id == url_id)
     return url_id
 
